acc2402ec04eeaed3a96176d7c62c7aab35f6707
Vitalliy Tsenyov <138374631+ViTseAlly@users.noreply.github.com>
2023-12-11 14:21:02 +0200

    Update main.cpp

--- LinkedList(singly_linked)/main.cpp
+++ LinkedList(singly_linked)/main.cpp
@@ -1,66 +1,145 @@
 #include <iostream>
 
-struct Node {
-    char current;
-    Node* next;
-    Node() {};
-    Node(char curV, Node* nextV) : current(curV), next(nextV) {};
+struct Node
+{
+  char current;
+  Node* next;
+  Node* prev;
+  Node() : current(char()), next(nullptr), prev(nullptr) {}
+  Node(Node* prevV, char curV, Node* nextV) : current(curV), next(nextV), prev(prevV) {}
 };
 
-class LinkedList {
+class LinkedList
+{
 public:
-    LinkedList() : node(nullptr) {};
-    ~LinkedList();
-    void addNode(char value);
-    void getNodes();
+  LinkedList() : _nodes(nullptr) {}
+  ~LinkedList();
+  void setValue(char _value);
+  void deleteValue();
+  void deleteValue(char _value);
+  void getList();
+
 private:
-    Node* node;
+  Node* _nodes;
 };
 
-LinkedList::~LinkedList() {
-    Node* current = node;
-    while (current != nullptr) {
-        Node* next = current->next;
-        delete current;
-        current = next;
-    }
+LinkedList::~LinkedList()
+{
+  Node* current = _nodes;
+  while (current != nullptr)
+  {
+    Node* next = current->next;
+    delete current;
+    current = next;
+  }
 }
 
-void LinkedList::addNode(char value) {
-    Node* cur = new Node(value, nullptr);
-    if (node == nullptr) node = cur;
-    else {
-        Node* current = node;
-        while (current->next != nullptr) {
-            current = current->next;
-        }
-        current->next = cur;
+void LinkedList::setValue(char _value)
+{
+  Node* newNode = new Node(nullptr, _value, nullptr);
+  if (_nodes == nullptr)
+  {
+    _nodes = newNode;
+  }
+  else
+  {
+    Node* current = _nodes;
+    while (current->next != nullptr)
+    {
+      current = current->next;
     }
+    current->next = newNode;
+    newNode->prev = current;
+  }
 }
 
-void LinkedList::getNodes() {
-    Node* value = node;
-    while (value != nullptr) {
-        std::cout << value->current;
-        value = value->next;
-    }
+void LinkedList::getList()
+{
+  Node* current = _nodes;
+  while (current != nullptr)
+  {
+    std::cout << current->current << " ";
+    current = current->next;
+  }
 }
 
-int main() {
-    LinkedList linkedList;
-    linkedList.addNode('H');
-    linkedList.addNode('e');
-    linkedList.addNode('l');
-    linkedList.addNode('l');
-    linkedList.addNode('o');
-    linkedList.addNode(' ');
-    linkedList.addNode('w');
-    linkedList.addNode('o');
-    linkedList.addNode('r');
-    linkedList.addNode('l');
-    linkedList.addNode('d');
-    linkedList.addNode('!');
+void LinkedList::deleteValue()
+{
+  if (_nodes == nullptr)
+  {
+    return;
+  }
 
-    linkedList.getNodes();
-    return 0;
+  Node* current = _nodes;
+  while (current->next != nullptr)
+  {
+    current = current->next;
+  }
+
+  if (current->prev != nullptr)
+  {
+    current->prev->next = nullptr; 
+  }
+  else
+  {
+    _nodes = nullptr; 
+  }
+
+  delete current;
+}
+
+void LinkedList::deleteValue(char _value)
+{
+  Node* current = _nodes;
+  while (current != nullptr && current->current != _value)
+  {
+    current = current->next;
+  }
+
+  if (current == nullptr)
+  {
+    std::cout << "Value not found in the list" << std::endl;
+    return;
+  }
+
+  if (current->prev != nullptr)
+  {
+    current->prev->next = current->next;
+  }
+  else
+  {
+    _nodes = current->next;
+  }
+
+  if (current->next != nullptr)
+  {
+    current->next->prev = current->prev;
+  }
+
+  delete current;
+}
+
+int main()
+{
+  LinkedList li;
+  li.setValue('H');
+  li.setValue('e');
+  li.setValue('l');
+  li.setValue('l');
+  li.setValue('o');
+  li.setValue(' ');
+  li.setValue('w');
+  li.setValue('o');
+  li.setValue('r');
+  li.setValue('l');
+  li.setValue('d');
+  li.setValue('!');
+  li.deleteValue();
+  li.deleteValue();
+  li.deleteValue();
+  li.deleteValue();
+  li.deleteValue('l');
+  li.getList();
+
+  return 0;
 }
